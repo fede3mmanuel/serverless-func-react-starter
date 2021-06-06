c37e801f7dd1b21e566016279dc062db4d3525b3
Fede <fede.fede.website>
2021-06-06 00:15:31 -0500

    React router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Product from './Product'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route  path="/" exact>
+        <App/>
+      </Route>
+      <Route  path="/:productID">
+        <Product/>
+      </Route>
+    </Router>
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 )
